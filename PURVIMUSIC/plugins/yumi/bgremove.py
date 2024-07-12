967aceb7ba80283bec294b72ec5840ed9d417d51
UTTAM RATHORE <uk977061@gmail.com>
2024-07-12 11:08:52 +0530

    Update bgremove.py

--- PURVIMUSIC/plugins/yumi/bgremove.py
+++ PURVIMUSIC/plugins/yumi/bgremove.py
@@ -2,7 +2,7 @@ import os
 import aiohttp
 import aiofiles
 from aiohttp import ContentTypeError
-from PURVIMUSIC import app as app
+from BABYMUSIC import app as app
 from pyrogram import filters
 
 def check_filename(filroid):
