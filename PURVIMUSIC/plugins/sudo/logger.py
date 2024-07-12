66edaaa082a13b65ea2f7f2cd81b668a16d3f213
UTTAM RATHORE <uk977061@gmail.com>
2024-07-12 12:37:45 +0530

    Update logger.py

--- PURVIMUSIC/plugins/sudo/logger.py
+++ PURVIMUSIC/plugins/sudo/logger.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 
-from PURVIMUSIC import app
-from PURVIMUSIC.misc import SUDOERS
-from PURVIMUSIC.utils.database import add_off, add_on
-from PURVIMUSIC.utils.decorators.language import language
+from BABYMUSIC import app
+from BABYMUSIC.misc import SUDOERS
+from BABYMUSIC.utils.database import add_off, add_on
+from BABYMUSIC.utils.decorators.language import language
 
 
 @app.on_message(filters.command(["logger"]) & SUDOERS)
