0d2dc761c82bb2c1a39798a307e0d26b42ca0ed9
UTTAM RATHORE <uk977061@gmail.com>
2024-07-12 13:06:58 +0530

    Update ytsearch.py

--- PURVIMUSIC/plugins/bot/ytsearch.py
+++ PURVIMUSIC/plugins/bot/ytsearch.py
@@ -1,7 +1,7 @@
 import logging
 from pyrogram.types import Message
 from youtube_search import YoutubeSearch
-from PURVIMUSIC import app
+from BABYMUSIC import app
 from pyrogram import filters
 from config import BOT_USERNAME
 
