8d148201a468024488e88389bb5fea971fc1f197
UTTAM RATHORE <uk977061@gmail.com>
2024-07-12 13:57:15 +0530

    Update Telegram.py

--- PURVIMUSIC/platforms/Telegram.py
+++ PURVIMUSIC/platforms/Telegram.py
@@ -6,8 +6,8 @@ from typing import Union
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Voice
 
 import config
-from PURVIMUSIC import app
-from PURVIMUSIC.utils.formatters import (
+from BABYMUSIC import app
+from BABYMUSIC.utils.formatters import (
     check_duration,
     convert_bytes,
     get_readable_time,
