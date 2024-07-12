f4076aea5567ec4c688ea036bcef491043e7e213
UTTAM RATHORE <uk977061@gmail.com>
2024-07-12 13:47:40 +0530

    Update unbanall.py

--- PURVIMUSIC/plugins/admins/unbanall.py
+++ PURVIMUSIC/plugins/admins/unbanall.py
@@ -1,4 +1,4 @@
-from PURVIMUSIC import app
+from BABYMUSIC import app
 from config import OWNER_ID
 from pyrogram import filters, enums
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
