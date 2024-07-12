9ad361bbd0a0237a31ceb918487a7d5d7b2b6677
UTTAM RATHORE <uk977061@gmail.com>
2024-07-12 12:49:07 +0530

    Update state.py

--- PURVIMUSIC/plugins/misc/state.py
+++ PURVIMUSIC/plugins/misc/state.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 import pycountry
-from PURVIMUSIC import app 
+from BABYMUSIC import app 
 
 @app.on_message(filters.command("get_states"))
 def get_states(client, message):
