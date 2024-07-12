aac020d36aee991042f03c6727f77433ba2b930d
UTTAM RATHORE <uk977061@gmail.com>
2024-07-12 11:37:59 +0530

    Update language.py

--- PURVIMUSIC/plugins/tools/language.py
+++ PURVIMUSIC/plugins/tools/language.py
@@ -2,9 +2,9 @@ from pykeyboard import InlineKeyboard
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, Message
 
-from PURVIMUSIC import app
-from PURVIMUSIC.utils.database import get_lang, set_lang
-from PURVIMUSIC.utils.decorators import ActualAdminCB, language, languageCB
+from BABYMUSIC import app
+from BABYMUSIC.utils.database import get_lang, set_lang
+from BABYMUSIC.utils.decorators import ActualAdminCB, language, languageCB
 from config import BANNED_USERS
 from strings import get_string, languages_present
 
