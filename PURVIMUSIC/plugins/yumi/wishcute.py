afc710e943d6509a7c54e50ee241adb14fb507a1
UTTAM RATHORE <uk977061@gmail.com>
2024-07-12 10:54:43 +0530

    Update wishcute.py

--- PURVIMUSIC/plugins/yumi/wishcute.py
+++ PURVIMUSIC/plugins/yumi/wishcute.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 import random
 import requests
-from PURVIMUSIC import app 
+from BABYMUSIC import app 
 
 SUPPORT_CHAT = "UBoT_HeLP"
 
