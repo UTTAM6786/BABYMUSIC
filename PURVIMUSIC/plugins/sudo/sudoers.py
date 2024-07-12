c00fd631b99261f0171802621e6ed9e338a86f05
UTTAM RATHORE <uk977061@gmail.com>
2024-07-12 12:31:39 +0530

    Update sudoers.py

--- PURVIMUSIC/plugins/sudo/sudoers.py
+++ PURVIMUSIC/plugins/sudo/sudoers.py
@@ -53,22 +53,22 @@ async def sudoers_list(client, message: Message):
     keyboard = [[InlineKeyboardButton("๏ ᴠɪᴇᴡ sᴜᴅᴏʟɪsᴛ ๏", callback_data="check_sudo_list")]]
     reply_markups = InlineKeyboardMarkup(keyboard)
   
-    #await message.reply_photo(photo="https://telegra.ph/file/2fae241ce04a9009d4a0c.mp4", caption="**» ᴄʜᴇᴄᴋ sᴜᴅᴏ ʟɪsᴛ ʙʏ ɢɪᴠᴇɴ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴ.**\n\n**» ɴᴏᴛᴇ:**  ᴏɴʟʏ sᴜᴅᴏ ᴜsᴇʀs ᴄᴀɴ ᴠɪᴇᴡ. ", reply_markup=reply_markups)
-    await message.reply_video(video="https://telegra.ph/file/2fae241ce04a9009d4a0c.mp4", caption="**» ᴄʜᴇᴄᴋ sᴜᴅᴏ ʟɪsᴛ ʙʏ ɢɪᴠᴇɴ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴ.**\n\n**» ɴᴏᴛᴇ:**  ᴏɴʟʏ sᴜᴅᴏ ᴜsᴇʀs ᴄᴀɴ ᴠɪᴇᴡ. ", reply_markup=reply_markups)
+    #await message.reply_photo(photo="https://telegra.ph/file/2fae241ce04a9009d4a0c.mp4", caption="**» ƈԋҽαƙ ʂυԃσ ʅιʂƚ Ⴆყ ɠιʋҽɳ Ⴆҽʅσɯ Ⴆυƚƚσɳ.**\n\n**» ɴᴏᴛᴇ:**  σɳʅყ ʂυԃσ υʂҽɾʂ ƈαɳ ʋιҽɯ. ", reply_markup=reply_markups)
+    await message.reply_video(video="https://telegra.ph/file/2fae241ce04a9009d4a0c.mp4", caption="**» ƈԋҽαƙ ʂυԃσ ʅιʂƚ Ⴆყ ɠιʋҽɳ Ⴆҽʅσɯ Ⴆυƚƚσɳ.**\n\n**» ɴᴏᴛᴇ:**  σɳʅყ ʂυԃσ υʂҽɾʂ ƈαɳ ʋιҽɯ. ", reply_markup=reply_markups)
     
 
 @app.on_callback_query(filters.regex("^check_sudo_list$"))
 async def check_sudo_list(client, callback_query: CallbackQuery):
     keyboard = []
     if callback_query.from_user.id not in SUDOERS:
-        return await callback_query.answer("𝐒𝐮𝐝𝐨 𝐋𝐢𝐬𝐭 𝐊 𝐁𝐚𝐝𝐥𝐞 𝐑𝐚𝐮𝐬𝐡𝐚𝐧 𝐊𝐚 𝐋𝐮𝐧𝐝 𝐋𝐞 𝐋𝐞🖕😎😂", show_alert=True)
+        return await callback_query.answer("𝐒𝐔𝐃𝐎 𝐋𝐈𝐒𝐓 𝐊 𝐁𝐀𝐃𝐋𝐄 𝐔𝐓𝐓𝐀𝐌 𝐊𝐄 𝐋𝐔𝐍𝐃 𝐋𝐄 𝐋𝐄🖕😎😂", show_alert=True)
     else:
         user = await app.get_users(OWNER_ID)
 
         user_mention = (user.first_name if not user.mention else user.mention)
-        caption = f"**˹ʟɪsᴛ ᴏғ ʙᴏᴛ ᴍᴏᴅᴇʀᴀᴛᴏʀs˼**\n\n**🌹Oᴡɴᴇʀ** ➥ {user_mention}\n\n"
+        caption = f"**ʅιʂƚ σϝ ɱσԃҽɾαƚσɾ**\n\n**🌹σɯɳҽɾ** ➥ {user_mention}\n\n"
 
-        keyboard.append([InlineKeyboardButton("๏ ᴠɪᴇᴡ ᴏᴡɴᴇʀ ๏", url=f"tg://openmessage?user_id={OWNER_ID}")])
+        keyboard.append([InlineKeyboardButton("๏ ʋιҽɯ σɯɳҽɾ ๏", url=f"tg://openmessage?user_id={OWNER_ID}")])
         
         count = 1
         for user_id in SUDOERS:
@@ -76,8 +76,8 @@ async def check_sudo_list(client, callback_query: CallbackQuery):
                 try:
                     user = await app.get_users(user_id)
                     user_mention = user.mention if user else f"**🎁 Sᴜᴅᴏ {count} ɪᴅ:** {user_id}"
-                    caption += f"**🎁 Sᴜᴅᴏ** {count} **»** {user_mention}\n"
-                    button_text = f"๏ ᴠɪᴇᴡ sᴜᴅᴏ {count} ๏ "
+                    caption += f"**🎁 ʂυԃσ** {count} **»** {user_mention}\n"
+                    button_text = f"๏ ʋιҽɯ ʂυԃσ {count} ๏ "
                     keyboard.append([InlineKeyboardButton(button_text, url=f"tg://openmessage?user_id={user_id}")]
                     )
                     count += 1
@@ -85,7 +85,7 @@ async def check_sudo_list(client, callback_query: CallbackQuery):
                     continue
 
         # Add a "Back" button at the end
-        keyboard.append([InlineKeyboardButton("๏ ʙᴀᴄᴋ ๏", callback_data="back_to_main_menu")])
+        keyboard.append([InlineKeyboardButton("๏ Ⴆαƈƙ ๏", callback_data="back_to_main_menu")])
 
         if keyboard:
             reply_markup = InlineKeyboardMarkup(keyboard)
@@ -95,7 +95,7 @@ async def check_sudo_list(client, callback_query: CallbackQuery):
 async def back_to_main_menu(client, callback_query: CallbackQuery):
     keyboard = [[InlineKeyboardButton("๏ ᴠɪᴇᴡ sᴜᴅᴏʟɪsᴛ ๏", callback_data="check_sudo_list")]]
     reply_markupes = InlineKeyboardMarkup(keyboard)
-    await callback_query.message.edit_caption(caption="**» ᴄʜᴇᴄᴋ sᴜᴅᴏ ʟɪsᴛ ʙʏ ɢɪᴠᴇɴ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴ.**\n\n**» ɴᴏᴛᴇ:**  ᴏɴʟʏ sᴜᴅᴏ ᴜsᴇʀs ᴄᴀɴ ᴠɪᴇᴡ. ", reply_markup=reply_markupes)
+    await callback_query.message.edit_caption(caption="**» ƈԋҽαƙ ʂυԃσ ʅιʂƚ Ⴆყ ɠιʋҽɳ Ⴆҽʅσɯ Ⴆυƚƚσɳ.**\n\n**» ɴᴏᴛᴇ:**  σɳʅყ ʂυԃσ υʂҽɾʂ ƈαɳ ʋιҽɯ. ", reply_markup=reply_markupes)
 
 
 
