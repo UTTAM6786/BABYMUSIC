4b80633714469bb5104b06d02f779856bfa55cc5
UTTAM RATHORE <uk977061@gmail.com>
2024-07-12 12:39:43 +0530

    Update blchat.py

--- PURVIMUSIC/plugins/sudo/blchat.py
+++ PURVIMUSIC/plugins/sudo/blchat.py
@@ -1,10 +1,10 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from PURVIMUSIC import app
-from PURVIMUSIC.misc import SUDOERS
-from PURVIMUSIC.utils.database import blacklist_chat, blacklisted_chats, whitelist_chat
-from PURVIMUSIC.utils.decorators.language import language
+from BABYMUSIC import app
+from BABYMUSIC.misc import SUDOERS
+from BABYMUSIC.utils.database import blacklist_chat, blacklisted_chats, whitelist_chat
+from BABYMUSIC.utils.decorators.language import language
 from config import BANNED_USERS
 
 
