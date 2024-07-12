25f45e979ee97e6c1837f4b548f0005d6c879555
UTTAM RATHORE <uk977061@gmail.com>
2024-07-12 10:44:13 +0530

    Update language.py

--- PURVIMUSIC/utils/decorators/language.py
+++ PURVIMUSIC/utils/decorators/language.py
@@ -1,5 +1,5 @@
-from PURVIMUSIC.misc import SUDOERS
-from PURVIMUSIC.utils.database import get_lang, is_maintenance
+from BABYMUSIC.misc import SUDOERS
+from BABYMUSIC.utils.database import get_lang, is_maintenance
 from strings import get_string
 
 
